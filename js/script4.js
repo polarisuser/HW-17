ea54b347dd94bb8291ac65d3d68cd825407ec77a
Aiperi Myimanbai <aiperi.myimanbaikyzy.pls@gmail.com>
2022-03-16 20:34:51 +0600

    corrected

--- js/script4.js
+++ js/script4.js
@@ -1,6 +1,6 @@
 let arr = ["Иван", "Иванов", "отдел разработки", "программист", 2000];
 
-let [name, surname, ...rest] = arr;
+let [name, surname, ...info] = arr;
 console.log(name);
 console.log(surname);
-console.log(rest);
+console.log(info);
